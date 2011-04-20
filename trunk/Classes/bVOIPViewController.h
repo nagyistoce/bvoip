2bed32148034e428f35a70adc773f8c8910f281c
begeesy@hotmail.com <begeesy@hotmail.com@970f95f6-d5a2-f342-cecd-8a05dc2e823f>
2011-04-20 15:55:08 +0000

    View Controller Header

---
+++ trunk/Classes/bVOIPViewController.h
@@ -0,0 +1,35 @@
+//
+//  bVOIPViewController.h
+//  bVOIP
+//
+//Copyright (C) 2011  Brian Green
+
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either version 2
+//of the License, or (at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bVOIPViewController : UIViewController {
+	IBOutlet UILabel *dialField;
+	int ID;
+	IBOutlet UIButton *Button;
+}
+
+-(IBAction)digitPressed:(id)sender;
+-(IBAction)clearText:(id)sender;
+-(IBAction)call:(id)sender;
+
+@end
+
